4843c2e3635b0c02618528c3a8b6c9761adc9774
BrendanZhang <ex.arc.zhang@gmail.com>
2021-01-19 22:03:22 +0800

    finish zh2en

--- src/colorLog.ts
+++ src/colorLog.ts
@@ -28,9 +28,12 @@ const en2zh = (obj: youdaoResult) => {
       );
   console.log("");
   obj.web &&
-    (console.log(cyanBright("短语")),
-    obj.web.map((webExplain) => {
-      console.log(blue(webExplain.key) + " " + blueBright(webExplain.value.join(" ; ")));
+    (console.log(cyanBright("网络释义")),
+    console.log(blue(obj.web[0].key) + " " + blueBright(obj.web[0].value.join(" ; "))),
+    console.log(cyanBright("短语")),
+    console.log(""),
+    obj.web.map((webExplain, index) => {
+      index !== 0 && console.log(blue(webExplain.key) + " " + blueBright(webExplain.value.join(" ; ")));
     }));
   console.log("");
   console.log(grey(obj.basic.exam_type.join(" ")));
@@ -39,7 +42,24 @@ const en2zh = (obj: youdaoResult) => {
 const zh2en = (obj: youdaoResult) => {
   console.log(obj);
   console.log("");
-  console.log(orange(obj.returnPhrase));
+  console.log(`${orange(obj.returnPhrase)} `);
+  obj.basic.phonetic && console.log(yellow(`[${obj.basic.phonetic}]`));
+  !obj.basic && console.log(blueBright(obj.translation));
+  console.log("");
+  obj.basic &&
+    obj.basic.explains.map((explain) => {
+      console.log(blue(explain));
+    });
+  console.log("");
+  obj.web &&
+    (console.log(cyanBright("网络释义")),
+    console.log(blue(obj.web[0].key) + " " + blueBright(obj.web[0].value.join(" ; "))),
+    console.log(""),
+    console.log(cyanBright("短语")),
+    obj.web.map((webExplain, index) => {
+      index !== 0 && console.log(blue(webExplain.key) + " " + blueBright(webExplain.value.join(" ; ")));
+    }));
+  console.log("");
 };
 
 const notWord = () => {
